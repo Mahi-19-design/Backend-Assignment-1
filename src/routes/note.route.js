e8d37bdd8f63cb5789ec0a7fe0f9d33d692571e0
mahi patel <mahipatelmp019@gmail.com>
2026-04-17 16:35:40 +0530

    final routes added

--- src/routes/note.route.js
+++ src/routes/note.route.js
@@ -1,3 +1,21 @@
-const express = require('express')
+
+
+ const express = require('express')
 const router = express.Router();
+
+
+const {create , createAll , read , readOne , update , deleteNote , deleteMany} = require('../controllers/note.controller')
+
+
+router.post('/notes' , create);
+router.post('/notes/bulk' , createAll);
+router.get('/notes',read);
+router.get('/notes/:id' , readOne);
+router.put('/notes/:id' , update);
+router.delete('/notes/:id' , deleteNote);
+router.delete('/notes/bulk' , deleteMany);
+
+
+module.exports = router;
+
 
